0cc6b798f323fa788e987aa59d5b8466775cfdc3
StenSOn27 <tarashevchik27@gmail.com>
2025-11-24 02:53:34 +0200

    add not null constraint to payment.booking_id

--- init_db/01-init_db.sql
+++ init_db/01-init_db.sql
@@ -85,7 +85,7 @@ CREATE TABLE trip (
 CREATE TABLE payment (
     payment_id SERIAL PRIMARY KEY,
     trip_id INT REFERENCES trip(trip_id),
-    booking_id INT REFERENCES booking(booking_id),
+    booking_id INT NOT NULL REFERENCES booking(booking_id),
     user_id INT NOT NULL REFERENCES users(user_id),
     amount NUMERIC(10,2) NOT NULL CHECK (amount >= 0),
     method payment_method NOT NULL,
--- init_db/09-insert_payment.sql
+++ init_db/09-insert_payment.sql
@@ -3,5 +3,4 @@ INSERT INTO payment (trip_id, booking_id, user_id, amount, method, status) VALUE
 (2, 2, 2, 20.00, 'card', 'pending'),
 (3, 3, 3, 30.00, 'cash', 'paid'),
 (4, 4, 4, 70.00, 'card', 'failed'),
-(5, 5, 5, 22.00, 'card', 'paid'),
-(6, NULL, 6, 60.00, 'card', 'paid');
+(5, 5, 5, 22.00, 'card', 'paid');

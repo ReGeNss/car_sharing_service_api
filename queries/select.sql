8aaa9e40f9e79e6aedf1cb069d2498327fee900c
Taras Shevchyk <tarashevchik27@gmail.com>
2025-11-24 20:54:41 +0200

    add selects

--- queries/select.sql
+++ queries/select.sql
@@ -5,3 +5,11 @@ SELECT * FROM vehicle WHERE status = 'available';
 -- 2. Знайти всіх активних користувачів
 
 SELECT * FROM users WHERE status = 'active';
+
+-- 3. Знайти всі тарифи, вартість яких > 6
+
+SELECT * FROM tariff WHERE price_per_minute > 6;
+
+-- 4. Знайти всі бронювання всіх активних користувачів
+
+SELECT * FROM booking WHERE user_id IN (SELECT user_id FROM users WHERE status = 'active');

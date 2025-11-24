f5606e3c55e433577c4d8a189355d7c794e82cc3
Taras Shevchyk <tarashevchik27@gmail.com>
2025-11-24 20:56:08 +0200

    add updates

--- queries/update.sql
+++ queries/update.sql
@@ -11,3 +11,18 @@ UPDATE users
 SET phone = '+380509991122'
 WHERE user_id = 1;
 SELECT * FROM users WHERE user_id = 1;
+
+-- 3. Оновити тариф (ціна, якщо пробіг <20)
+SELECT * FROM tariff WHERE tariff_id = 2 AND included_mileage < 20;
+UPDATE tariff
+SET price_per_minute = 8.00
+WHERE tariff_id = 2 AND included_mileage < 20;
+SELECT * FROM tariff WHERE tariff_id = 2 AND included_mileage < 20;
+
+-- 4. Проставити завершення бронювання
+SELECT * FROM booking WHERE booking_id = 1;
+UPDATE booking
+SET status = 'expired',
+    end_time = NOW()
+WHERE booking_id = 1;
+SELECT * FROM booking WHERE booking_id = 1;

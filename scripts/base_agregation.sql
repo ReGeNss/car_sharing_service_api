fc307bb5dc48267152cd91c83e8be8cdc66cbbdb
Максим Повнич <maxympovnych@gmail.com>
2025-11-25 00:42:35 +0200

    create base agregation oltp

--- scripts/base_agregation.sql
+++ scripts/base_agregation.sql
@@ -3,3 +3,12 @@ FROM penalty;
 
 SELECT AVG(mileage) AS average_maintenance_mileage
 FROM maintenance;
+
+SELECT booking_price, COUNT(*) AS tariff_count
+FROM tariff
+WHERE deposit > 1000
+GROUP BY booking_price;
+
+
+SELECT MIN(price_per_minute) AS min_tariff_price
+FROM tariff;
